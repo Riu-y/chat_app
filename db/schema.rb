bf28010ed8631ddaf1df679ed64f60dc2f322a22
Riu-y <y1a1r2y5u@gmail.com>
2020-05-26 15:07:40 +0000

    [Add]チャンネルとJqueryの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_135530) do
+ActiveRecord::Schema.define(version: 2020_05_26_150000) do
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "room_id"
   end
 
   create_table "rooms", force: :cascade do |t|
